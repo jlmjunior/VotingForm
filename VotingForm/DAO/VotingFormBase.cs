5e101083a142affd25e7c34323a456c3019f29fb
Junior Martinez <jlmjunior@live.com>
2020-05-18 23:50:44 -0300

    Criação do DAO

---
+++ VotingForm/DAO/VotingFormBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace VotingForm.DAO
+{
+    public class VotingFormBase
+    {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings[""].ConnectionString);
+
+        public DataTable GetPoll(string idPoll)
+        {
+            return null;
+        }
+    }
+}
